515091b0a51a777f09b28835cbfd1575058ac5e1
Konrad <wesolowskikonrad28@gmail.com>
2020-06-30 13:40:43 +0200

    products list

--- src/main/java/spring_mvc/demo/Controllers/IndexController.java
+++ src/main/java/spring_mvc/demo/Controllers/IndexController.java
@@ -1,4 +1,13 @@
 package spring_mvc.demo.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class IndexController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
 }
---
+++ src/main/java/spring_mvc/demo/Services/ProductService.java
@@ -0,0 +1,9 @@
+package spring_mvc.demo.Services;
+
+import spring_mvc.demo.Domain.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    List<Product> listAllProducts();
+}
